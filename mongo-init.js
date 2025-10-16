2d9fd5046da9e4cd6a6f3c4970fbeafada887e16
Md. Tazwarul Islam Abir <121948453+Abir-7@users.noreply.github.com>
2025-10-16 14:19:53 +0600

    Update mongo-init.js

--- mongo-init.js
+++ mongo-init.js
@@ -1,18 +1,16 @@
 // mongo-init.js
-// This script will auto-initialize replica set if not already initialized
-rsConf = {
-  _id: "rs0",
-  members: [
-    { _id: 0, host: "mongo1:27017" },
-    { _id: 1, host: "mongo2:27017" },
-    { _id: 2, host: "mongo3:27017" }
-  ]
-};
-
 try {
-  var status = rs.status();
+  const status = rs.status();
   print("Replica set already initialized");
 } catch (e) {
-  print("Initializing replica set...");
-  rs.initiate(rsConf);
+  print("Initializing replica set with mongo1 as primary...");
+
+  rs.initiate({
+    _id: "rs0",
+    members: [
+      { _id: 0, host: "mongo1:27017", priority: 2 },
+      { _id: 1, host: "mongo2:27017", priority: 1 },
+      { _id: 2, host: "mongo3:27017", priority: 1 }
+    ]
+  });
 }
